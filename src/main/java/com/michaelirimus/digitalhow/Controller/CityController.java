2b38fde203fce7b480468622e36034530e2d022e
Michael Irimus <michaelirimus@gmail.com>
2022-05-26 17:56:38 +0200

    add pagination to cities request mapping

--- src/main/java/com/michaelirimus/digitalhow/Controller/CityController.java
+++ src/main/java/com/michaelirimus/digitalhow/Controller/CityController.java
@@ -3,7 +3,9 @@ package com.michaelirimus.digitalhow.Controller;
 import com.michaelirimus.digitalhow.Model.City;
 import com.michaelirimus.digitalhow.Repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,8 @@ public class CityController {
     private CityRepository cityRepository;
 
     @GetMapping("/cities")
-    public List<City> getCities() {
-        return cityRepository.findAll();
+    public List<City> getCities(@RequestParam(name = "page") int page,
+                                @RequestParam(name = "size") int size) {
+        return cityRepository.findAll(Pageable.ofSize(size).withPage(page)).getContent();
     }
 }
